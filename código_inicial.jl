3e6ec07fe6b28e527e0924444175ee0c495d9545
clahcec <clarisse.cecconi@estudante.ufscar.br>
2021-08-01 23:03:58 -0300

    last edition

--- código_inicial.jl
+++ código_inicial.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra: sqrt
 using Distances
 using LinearAlgebra
 using LaTeXStrings
@@ -15,37 +16,45 @@ using FileIO
 using JLD2
 using Dates
 
-k=1
+k=1 #modulo do vetor de onda (ou número de onda) 
+Γ₀
 
 x=15
 y=20
 z=10
 ###################################variáveis do comprimento, largura e altura da nuvem
 N=500 #número de átomos na nuvem
-ρ =N/(x*y*z)#eq da densidade
+ρ=N/(x*y*z)#eq da densidade
 r=zeros(N,3) #matriz da posição dos átomos: (x,y,z) de cada N átomos - matriz com N linhas e 3 colunas: x,y,z
-X_atoms=rand(N)*x .-x/2
-Y_atoms=rand(N)*y .-y/2
-Z_atoms=rand(N)*z .-z/2
+X_atoms=rand(N)*x .-x/2 #gerando N valores aleatórios no intervalo [-x/2,x/2] ------ (. = reduzindo todo mundo ao mesmo comando)
+Y_atoms=rand(N)*y .-y/2 #gerando N valores aleatórios no intervalo [-y/2,y/2] ------ (. = reduzindo todo mundo ao mesmo comando)
+Z_atoms=rand(N)*z .-z/2 #gerando N valores aleatórios no intervalo [-z/2,z/2] ------ (. = reduzindo todo mundo ao mesmo comando)
 
-r[:,1]=X_atoms
-r[:,2]=Y_atoms
-r[:,3]=Z_atoms
 
-tamanho=1000
-Limite=15
+#preenchendo da matriz da posição dos atomos (x,y,z)
+r[:,1]=X_atoms #preenchendo todas as linhas da coluna 1 da matriz r da posição dos atomos em x
+r[:,2]=Y_atoms #preenchendo todas as linhas da coluna 1 da matriz r da posição dos atomos em y
+r[:,3]=Z_atoms #preenchendo todas as linhas da coluna 1 da matriz r da posição dos atomos em z
+
+
+#plot
+tamanho=1000 #tamanho da figura
+Limite=12 #limite da
 
 gr()
 theme(:vibrant)
 
-
-scatter(r[:,1],r[:,2],r[:,3],
-size = (tamanho, tamanho),
+#função que plota um conjunto de pontos no espaço 3d (x,y,z), 2d(x,y) e 1d(x)
+scatter(
+r[:,1], #posição x dos atomos
+r[:,2], #posição y dos atomos
+r[:,3], #posição z dos atomos
+size = (tamanho, tamanho),#tamanho da figura (quadrado)
 left_margin = 10Plots.mm,
 right_margin = 12Plots.mm,
 top_margin = 5Plots.mm,
 bottom_margin = 5Plots.mm,
-c = :green,
+c = :green, #cor
 framestyle = :box,
 gridalpha = 0.3,
 xlims = (-(1.2)*Limite,(1.2)*Limite),
@@ -64,6 +73,46 @@ foreground_color_legend = :black
 )
 savefig("fig1__N={$N}__Densidade={$ρ}__K={$k}.png")
 
+ #=teste distância correta entre atomos
+while true 
+    X_atoms_1=rand()*x  
+    Y_atoms_1=rand()*y  
+    Z_atoms_1=rand()*z  
+    A= (X_atoms_1,Y_atoms_1,Z_atoms_1)
+
+    X_atoms_2=rand()*x  
+    Y_atoms_2=rand()*y  
+    Z_atoms_2=rand()*z  
+    B= (X_atoms_2,Y_atoms_2,Z_atoms_2)
+
+    m=zeros(2,3)
+    distância_entre_os_pontos= √((X_atoms_1-X_atoms_2)^2 + (Y_atoms_1-Y_atoms_2)^2 +(Z_atoms_1-Z_atoms_2)^2)
+    a=0     
+    while true
+    
+        if distância_entre_os_pontos >= 1/(10√(ρ)) , a<=N
+            m[1,1] = X_atoms_1
+            m[1,2] = Y_atoms_1
+            m[1,3] = Z_atoms_1
+            m[2,1] = X_atoms_2
+            m[2,2] = Y_atoms_2
+            m[2,3] = Z_atoms_2
+            a=a+1
+        else 
+            if a>N
+                break
+
+                
+
+
+        
+
+
+
+
+
+
+#=
 a=0
 
 for i in 1:10
@@ -79,3 +128,4 @@ while true
     end
 end
 a
+=#
